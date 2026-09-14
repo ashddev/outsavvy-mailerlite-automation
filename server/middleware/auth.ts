d19c63aa261fd02856c02481f03fb97135c63a36
Ash D <ashddev@proton.me>
2026-09-14 21:44:54 +0100

    chore: remove middleware when in prod env

--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -1,12 +1,14 @@
 export default defineEventHandler((event) => {
-  const auth = getRequestHeader(event, "authorization");
-  if (
-    !process.env.CRON_SECRET ||
-    auth !== `Bearer ${process.env.CRON_SECRET}`
-  ) {
-    throw createError({
-      status: 401,
-      statusMessage: "Unauthorized",
-    });
+  if (process.env.NODE_ENV === "production") {
+    const auth = getRequestHeader(event, "authorization");
+    if (
+      !process.env.CRON_SECRET ||
+      auth !== `Bearer ${process.env.CRON_SECRET}`
+    ) {
+      throw createError({
+        status: 401,
+        statusMessage: "Unauthorized",
+      });
+    }
   }
 });
